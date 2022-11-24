03f61bd1590acec6ebd7f8a9e6c36c0ae4db2b6c
narr0225 <nattakornn193@gmail.com>
2022-11-24 11:56:03 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ app.use(cors());
 // middelware
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../build')))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../build/index.html"))
-    })
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "../build/index.html"))
+//     })
+// }
 
 app.use((req,res,next)=>{
     console.log(req.path, req.method)
